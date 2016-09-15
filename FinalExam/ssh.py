f35bdd318d5fd89945e8f5cf4c2275be36188375
pyautomation <a>
2016-09-16 00:21:26 +0300

    After )

--- FinalExam/ssh.py
+++ FinalExam/ssh.py
@@ -6,113 +6,129 @@ import pickle
 import scaner as scan
 
 class SSH(object):
-    def __init__(self):
+    def __init__(self, username = 'pythonista', password = 'letmein', login = 'root'):
+        self.server = ''
+        self.ssh_port = ''
+        self.username = username
+        self.password = password
+        self.mysql_login = login
+        self.mysql_password = ''
+
         self.ssh = None
 
-    def get_ssh_con(self, host, port, username, password):
-        ssh = None
+    def get_server_info(self):
+        info = {
+            'server_ip':self.server,
+            'ssh_port':self.ssh_port,
+            'username':self.username,
+            'password':self.password
+        }
+        return info
+    def get_mysql_info(self):
+        info = {
+            'mysql_login':self.mysql_login,
+             'mysql_password':self.mysql_password
+        }
+        return info
+
+    def get_all_info(self):
+        server_info = self.get_server_info()
+        mysql_imfo = self.get_mysql_info()
+        server_info.update(mysql_imfo)
+        return server_info
+
+
+    def try_ssh_con(self, host, port, user, passw):
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(host, int(port), username=username, password=password)
+            ssh.connect(host, int(port), username=user, password=passw)
+            # ssh.close()
         except:
-            print('ssh: can not connect')
             ssh.close()
-            ssh = None
-        finally:
+            print('ssh: can not connect')
+            return None
+        else:
             self.ssh = ssh
             return ssh
 
-    # find server and port and ssh_connection
-    def find_server(self,username, password, struct_net):
+    # find server and port and ssh_connection in struct net
+    def find_server(self, **struct_net):
         ssh = None
         for host, ports in struct_net.items():
             for port in ports:
-                ssh = self.get_ssh_con(host, port, username, password)
+                ssh = self.try_ssh_con(host, port, self.username, self.password)
+                self.ssh = ssh
+                self.server = host
+                self.ssh_port = port
                 if ssh:
                     return host, port, ssh
-        return None, None, ssh
+        return None, None, None
 
-    def find_password(self, ssh_conn, user_mysql='root'):
+    def find_password(self, ssh_conn):
         stdin, stdout, stderr = ssh_conn.exec_command('grep -R password= *')
         data = stdout.read()
         pattern = re.compile('password=([a-zA-Z0-9]+)')
         return pattern.findall(data)
 
-def save_server_info_json(server_ip, ssh_port, login, password):
-    info = {
-        'ip':server_ip,
-        'ssh_port':ssh_port,
-        'login':login,
-        'password':password
-    }
-    json.dump(info, open('alesya_server.json', 'w'))
 
-def save_mysql_info_yaml(mysql_pass, mysql_login='root'):
-    info = {
-        'login':mysql_login,
-        'password':mysql_pass
-    }
-    yaml.dump(info, open('alesya_sql.yaml', 'w'))
+    def save_server_info_json(self, **server_info):
+        json.dump(server_info, open('alesya_server.json', 'w'))
 
+    def save_mysql_info_yaml(self, **mysql_info):
+        yaml.dump(mysql_info, open('alesya_sql.yaml', 'w'))
 
-def save_backup(server_ip, ssh_port, login, password, mysql_pass, mysql_login='root'):
-    info = {
-        'ip':server_ip,
-        'ssh_port':ssh_port,
-        'login':login,
-        'password':password,
-        'login_mysql':mysql_login,
-        'password_mysql':mysql_pass
-    }
-    pickle.dump(info, open('alesya_bin_backup.txt', 'w'))
+    def save_backup(self, **info):
+        pickle.dump(info, open('alesya_bin_backup.txt', 'w'))
 
-def copy_db_file(username, password, server, ssh_port):
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(server, port=int(ssh_port), username=username, password=password)
-        sftp = ssh.open_sftp()
-        path = '/home/{}/db.py'.format(username)
-        sftp.put('db.py', path)
-        sftp.close()
-        ssh.close()
-        return True
-    except:
-        print('Err: ssh copy file')
-        ssh.close()
-        return False
+    def copy_db_file(self):
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(self.server, port=int(self.ssh_port), username=self.username, password=self.password)
+            sftp = ssh.open_sftp()
+            path = '/home/{}/db.py'.format(self.username)
+            sftp.put('db.py', path)
+            sftp.close()
+            ssh.close()
+            return True
+        except:
+            print('Err: ssh copy file')
+            ssh.close()
+            return False
 
-def check_db(server_ip, ssh_port, username, password):
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(server, port=int(ssh_port), username=username, password=password)
-        command = 'python db.py root exam'.format(username)
-        stdin, stdout, stderr = ssh.exec_command(command)
-    except:
-        print('Error: ssh check db')
-    finally:
-        ssh.close()
+    def check_db(self):
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(self.server, port=int(self.ssh_port), username=self.username, password=self.password)
+            command = 'python db.py root exam'.format(self.username)
+            stdin, stdout, stderr = ssh.exec_command(command)
+        except:
+            print('Error: ssh check db')
+        finally:
+            ssh.close()
 
 if __name__ == "__main__" :
     struct_net = scan.ScanNet().get_struct_net()
-    username = 'pythonista'
-    password = 'letmein'
+    print(struct_net)
+    json.dump(struct_net, open('struct_net', 'w'))
+
     ssh = SSH()
 
     password_mysql = ''
-    server, ssh_port, ssh_con = ssh.find_server(username, password, struct_net)
+    server, ssh_port, ssh_con = ssh.find_server(**struct_net)
+    print( server, ssh_port, ssh_con)
 
     if server and ssh_port and ssh_con:
-        save_server_info_json(server, ssh_port, username, password)
+        ssh.save_server_info_json(**ssh.get_server_info())
         passwords = ssh.find_password(ssh_con)
         if len(passwords) != 0:
             password_mysql = passwords[0]
-        save_mysql_info_yaml(password_mysql)
-        save_backup(server, ssh_port, username, password, password_mysql)
+        ssh.save_mysql_info_yaml(**ssh.get_mysql_info())
+        ssh.save_backup(**ssh.get_all_info())
 
-    check_db(server, ssh_port, username, password)
+    ssh.check_db()
 
 
 
